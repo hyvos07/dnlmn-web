6995526e98a83458eef797e7c690c7e6980941e9
hyvos07 <daniel.liman07@gmail.com>
2025-04-02 22:56:59 +0700

    feat: add link to linkedin for more experiences

--- src/sections/4_experiences/index.tsx
+++ src/sections/4_experiences/index.tsx
@@ -10,6 +10,19 @@ export default function ExperienceContent() {
                 <h2 className="text-2xl lg:text-3xl font-semibold mb-12 md:mb-20 xl:mb-[90px] text-center">My Experiences</h2>
                 <div className="w-full">
                     <Timeline experiences={experiences} />
+                    <p className="font-semibold text-center max-md:text-sm text-lg mt-16 md:mt-28">
+                        More informations can be found on my &nbsp;
+                        <a
+                            href="https://www.linkedin.com/in/danielliman/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-[#54b4e2] relative inline-block group"
+                        >
+                            LinkedIn
+                            <span className="absolute left-0 bottom-0 h-[1px] bg-[#54b4e2] w-0 group-hover:w-full transition-all duration-300"></span>
+                        </a>
+                        &nbsp; profile.
+                    </p>
                 </div>
             </div>
         </div>
