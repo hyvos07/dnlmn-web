e56153ca1a8a3e3bb4b414f4e608f40a988dfbce
hyvos07 <daniel.liman07@gmail.com>
2026-07-08 23:20:03 +0700

    chore: update next and eslint-config-next to latest versions

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
   images: {
-    domains: [
-      'i.scdn.co',
-      'raw.githubusercontent.com',
-      'www.vectorlogo.zone',
-      'upload.wikimedia.org',
-      'www.svgrepo.com',
-      'user-images.githubusercontent.com',
-      'cdn.worldvectorlogo.com',
-      'github.com',
-      'miro.medium.com',
+    remotePatterns: [
+      { protocol: 'https', hostname: 'i.scdn.co' },
+      { protocol: 'https', hostname: 'raw.githubusercontent.com' },
+      { protocol: 'https', hostname: 'www.vectorlogo.zone' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: 'www.svgrepo.com' },
+      { protocol: 'https', hostname: 'user-images.githubusercontent.com' },
+      { protocol: 'https', hostname: 'cdn.worldvectorlogo.com' },
+      { protocol: 'https', hostname: 'github.com' },
+      { protocol: 'https', hostname: 'miro.medium.com' },
     ],
   },
 };
